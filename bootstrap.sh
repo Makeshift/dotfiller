a874687e1cbdf6e93ba1f5c901dee2cb56c57d2d
Connor Bell <git@connor-bell.com>
2020-08-03 12:30:13 +0000

    Bootstrap script, better readme

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,23 @@
 # You should copy this script and customise it to your liking, then host it somewhere you can easily grab.
 # I put mine on a webserver where I can just curl it, then run it to set up Dotfiller.
 
-OVERWRITE_ALL=1
-SYNC_CHOICE=cron
-USER_GIT_REPO=https://github.com/Makeshift/dotfiles
-GIT_PROVIDER_UPDATE=login
-DROPBOX_DIR=homedir
-SYMLINK_NOW=y
-CREATE_LINKS_WHEN=login
+mkdir -p "$HOME/.dotfiller/bin"
 
-mkdir -p "$HOME/.dotfiller"
+curl -s https://api.github.com/repos/Makeshift/dotfiller/releases/latest \
+| /bin/grep "zipball_url" \
+| cut -d : -f 2,3 \
+| tr -d ,\" \
+| xargs -I % curl -L -s -o "$HOME/.dotfiller/bin/dotfiller.zip" %
+
+#unzip -o -j "$HOME/.dotfiller/bin/dotfiller.zip" -d "$HOME/.dotfiller/bin/" > /dev/null
+rm "$HOME/.dotfiller/bin/dotfiller.zip" "$HOME/.dotfiller/bin/Readme.md"
+
+# There are likely cleaner ways to do this, but it works!
+OVERWRITE_ALL=1 \
+SYNC_CHOICE=cron \
+USER_GIT_REPO=https://github.com/Makeshift/dotfiles \
+GIT_PROVIDER_UPDATE=login \
+DROPBOX_DIR=homedir \
+SYMLINK_NOW=y \
+CREATE_LINKS_WHEN=login \
+"$HOME/.dotfiller/bin/dotfiller" install
